845c3d9f9985150e5ea7f77c87ea56ac1ddfc3d4
sbalsa <sbalsa22@outlook.com>
2020-09-06 14:54:41 +0200

    First Commit

---
+++ src/contact.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "please enter your name"<< std::endl;
+    //std::cin >> name; 
+    //std::out << "your name is: " << std::endl;
+    return 0;
+}
